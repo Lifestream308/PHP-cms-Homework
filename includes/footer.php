fc71dad84f4ef8341124d8d3d64615f58deae1e9
Christian Arroyo <ChristianArroyo308@yahoo.com>
2024-05-16 17:30:49 -0700

    initial commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+    <footer>
+      <div class="container">
+        &copy; Christian Arroyo, CISW 31, <?= date('Y'); ?>
+      </div>
+    </footer>
+    <script src="js/site.js"></script>
+  </body>
+</html>
